102f3946edc36f368f9831788fa9b607b11432f6
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-24 03:02:07 +0300

    defining timer functions

--- Lib/indexs.js
+++ Lib/indexs.js
@@ -18,4 +18,79 @@ window.onload=function () {
     let minS3 = document.getElementById("min3")
 
     let interval;
+
+
+
+
+
+
+
+
+   function stopWatch(){
+        ms++;
+        if (ms==100) {
+            ms=0;
+            sec++;
+            msS.innerHTML=ms;
+
+        }
+
+        if (sec==60) {
+            sec=0;
+            min++;
+            secS.innerHTML=sec;
+        }
+
+        if (ms<10) {
+            msS.innerHTML='0'+ms;
+        }
+        else {
+            msS.innerHTML=ms;
+        }
+
+        if (sec<10) {
+            secS.innerHTML='0'+sec;
+        }
+        else {
+            secS.innerHTML=sec;
+        }
+
+        if (min<10) {
+            minS.innerHTML='0'+min;
+        }
+        else {
+            minS.innerHTML=min;
+        }
+
+
+
+    }
+    function startTimer(){
+        clearInterval(interval);
+        interval=setInterval(stopWatch,10);
+    }
+
+    // function stoptimer() {
+    //     clearInterval(interval);
+    // }
+
+
+    function resetTimer() {
+        clearInterval(interval);
+        ms=0;
+        sec=0;
+        min=0;
+
+        msS.innerHTML=00;
+        secS.innerHTML=00;
+        minS.innerHTML=00;
+
+    }
+
+
+    function last(){
+        msS3.innerHTML=ms;
+        secS3.innerHTML=sec;
+        minS3.innerHTML=min;
+    }
 }
